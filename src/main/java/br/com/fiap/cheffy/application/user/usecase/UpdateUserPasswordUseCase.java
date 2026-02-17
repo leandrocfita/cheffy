e8cccef4c1eaa3e146fc9ad486f35947b72d52db
Igor Costa <icgsbr@gmail.com>
2026-02-17 09:29:09 -0300

    refactor: encode user password before saving

--- src/main/java/br/com/fiap/cheffy/application/user/usecase/UpdateUserPasswordUseCase.java
+++ src/main/java/br/com/fiap/cheffy/application/user/usecase/UpdateUserPasswordUseCase.java
@@ -3,6 +3,7 @@ package br.com.fiap.cheffy.application.user.usecase;
 import br.com.fiap.cheffy.application.user.dto.UserCommandPort;
 import br.com.fiap.cheffy.domain.user.entity.User;
 import br.com.fiap.cheffy.domain.user.exception.UserNotFoundException;
+import br.com.fiap.cheffy.domain.user.port.input.PasswordEncoderPort;
 import br.com.fiap.cheffy.domain.user.port.input.UpdateUserPasswordInput;
 import br.com.fiap.cheffy.domain.user.port.output.UserRepository;
 
@@ -13,17 +14,29 @@ import static br.com.fiap.cheffy.shared.exception.keys.ExceptionsKeys.USER_NOT_F
 public class UpdateUserPasswordUseCase implements UpdateUserPasswordInput {
 
     private final UserRepository userRepository;
+    private final PasswordEncoderPort passwordEncoderPort;
 
-    public UpdateUserPasswordUseCase(UserRepository userRepository) {
+    public UpdateUserPasswordUseCase(
+            UserRepository userRepository,
+            PasswordEncoderPort passwordEncoderPort
+    ) {
         this.userRepository = userRepository;
+        this.passwordEncoderPort = passwordEncoderPort;
     }
 
     public void execute(UserCommandPort command, UUID id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(USER_NOT_FOUND_EXCEPTION, id));
 
-        user.changePassword(command.password());
+        updatePassword(user, command.password());
 
         userRepository.save(user);
+    }
+
+    private void updatePassword(User user, String rawPassword) {
+        String encodedPassword = passwordEncoderPort.encode(rawPassword);
+
+        User.validatePassword(rawPassword);
+        user.setPassword(encodedPassword);
     }
 }
