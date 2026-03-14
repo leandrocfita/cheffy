6c20c3ebb99c565fbbd4d92afd8d712dc7188902
thiagosslima <thiagosslima@MacBook-Pro-M4-Thiago-Soares.local>
2026-03-14 16:20:23 -0300

    feat: adicionado testes

--- src/test/java/br/com/fiap/cheffy/application/profile/usecase/ListAllProfileUseCaseTest.java
+++ src/test/java/br/com/fiap/cheffy/application/profile/usecase/ListAllProfileUseCaseTest.java
@@ -1,6 +1,9 @@
 package br.com.fiap.cheffy.application.profile.usecase;
 
+import br.com.fiap.cheffy.application.profile.dto.PageInputPort;
+import br.com.fiap.cheffy.application.profile.dto.PageOutputPort;
 import br.com.fiap.cheffy.application.profile.dto.ProfileQueryPort;
+import br.com.fiap.cheffy.application.profile.dto.SortRequestPort;
 import br.com.fiap.cheffy.application.profile.mapper.ProfileQueryMapper;
 import br.com.fiap.cheffy.domain.profile.ProfileType;
 import br.com.fiap.cheffy.domain.profile.entity.Profile;
@@ -10,10 +13,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -33,66 +32,91 @@ class ListAllProfileUseCaseTest {
 
     private ListAllProfileUseCase useCase;
 
+    private int page;
+    private int size;
+    private SortRequestPort sort;
+
+    private Profile firstProfile;
+    private Profile secondProfile;
+
+    private ProfileQueryPort mappedFirstProfile;
+    private ProfileQueryPort mappedSecondProfile;
+
+    private List<Profile> profiles;
+
+    private PageInputPort pageInput;
+
     @BeforeEach
     void setUp() {
         useCase = new ListAllProfileUseCase(profileRepository, mapper);
+
+        this.page = 0;
+        this.size = 10;
+        this.sort = new SortRequestPort("type", SortRequestPort.Direction.ASC);
+
+        this.firstProfile = createProfile(1L, ProfileType.CLIENT.getType());
+        this.secondProfile = createProfile(2L, ProfileType.OWNER.getType());
+
+        this.mappedFirstProfile = new ProfileQueryPort(1L, ProfileType.CLIENT.getType());
+        this.mappedSecondProfile = new ProfileQueryPort(2L, ProfileType.OWNER.getType());
+
+        this.profiles = Arrays.asList(this.firstProfile, this.secondProfile);
+
+        pageInput = new PageInputPort(this.page, this.size, this.sort);
     }
 
-//    @Test
-//    void shouldListProfilesSuccessfully() {
-//        Profile firstProfile = createProfile(1L, ProfileType.CLIENT.getType());
-//        Profile secondProfile = createProfile(2L, ProfileType.OWNER.getType());
-//
-//        Pageable pageable = PageRequest.of(0, 10);
-//        Page<Profile> profilePage = new PageImpl<>(Arrays.asList(firstProfile, secondProfile), pageable, 2);
-//
-//        ProfileQueryPort mappedFirstProfile = mock(ProfileQueryPort.class);
-//        ProfileQueryPort mappedSecondProfile = mock(ProfileQueryPort.class);
-//
-//        when(profileRepository.findAll(pageable)).thenReturn(profilePage);
-//        when(mapper.toQuery(firstProfile)).thenReturn(mappedFirstProfile);
-//        when(mapper.toQuery(secondProfile)).thenReturn(mappedSecondProfile);
-//
-//        Page<ProfileQueryPort> profilesPageResult = useCase.execute(pageable);
-//
-//        assertNotNull(profilesPageResult);
-//        assertEquals(2, profilesPageResult.getTotalElements());
-//        assertEquals(2, profilesPageResult.getContent().size());
-//        verify(profileRepository, times(1)).findAll(pageable);
-//    }
-//
-//    @Test
-//    void shouldReturnEmptyListWhenNoProfilesFound() {
-//        Pageable pageable = PageRequest.of(0, 10);
-//        Page<Profile> emptyPage = new PageImpl<>(Collections.emptyList(), pageable, 0);
-//        when(profileRepository.findAll(pageable)).thenReturn(emptyPage);
-//
-//        Page<ProfileQueryPort> profilesPageResult = useCase.execute(pageable);
-//
-//        assertNotNull(profilesPageResult);
-//        assertEquals(0, profilesPageResult.getTotalElements());
-//        assertTrue(profilesPageResult.getContent().isEmpty());
-//    }
-//
-//    @Test
-//    void shouldHandlePagination() {
-//        Profile profile = createProfile(1L, ProfileType.CLIENT.getType());
-//
-//        Pageable pageable = PageRequest.of(1, 5);
-//        Page<Profile> profilePage = new PageImpl<>(List.of(profile), pageable, 10);
-//
-//        ProfileQueryPort query = mock(ProfileQueryPort.class);
-//        when(profileRepository.findAll(pageable)).thenReturn(profilePage);
-//        when(mapper.toQuery(profile)).thenReturn(query);
-//
-//        Page<ProfileQueryPort> result = useCase.execute(pageable);
-//
-//        assertEquals(10, result.getTotalElements());
-//        assertEquals(2, result.getTotalPages());
-//        assertEquals(1, result.getNumber());
-//    }
-//
-//    private Profile createProfile(Long id, String type) {
-//        return Profile.create(id, type);
-//    }
+    @Test
+    void shouldListProfilesSuccessfully() {
+
+        var profilePage = new PageOutputPort<Profile>(profiles, this.page, this.size, 2);
+
+        when(profileRepository.findAll(this.pageInput)).thenReturn(profilePage);
+
+        when(mapper.toQuery(this.firstProfile)).thenReturn(this.mappedFirstProfile);
+        when(mapper.toQuery(this.secondProfile)).thenReturn(this.mappedSecondProfile);
+
+        var result = useCase.execute(this.pageInput);
+
+        assertNotNull(result);
+        assertEquals(2, result.getTotalElements());
+        assertEquals(2, result.getContent().size());
+        verify(profileRepository, times(1)).findAll(this.pageInput);
+        assertEquals(ProfileType.CLIENT.getType(), result.getContent().getFirst().type());
+    }
+
+    @Test
+    void shouldListProfilesSuccessfullyWithDescending() {
+        var mappedFirstProfileExpected = new ProfileQueryPort(2L, ProfileType.OWNER.getType());
+        var mappedSecondProfileExpected = new ProfileQueryPort(1L, ProfileType.CLIENT.getType());
+
+        var profilePage = new PageOutputPort<Profile>(Arrays.asList(this.firstProfile, this.secondProfile), this.page, this.size, 2);
+
+        when(profileRepository.findAll(this.pageInput)).thenReturn(profilePage);
+        when(mapper.toQuery(any())).thenReturn(mappedFirstProfileExpected, mappedSecondProfileExpected);
+
+        var result = useCase.execute(this.pageInput);
+
+        assertNotNull(result);
+        assertEquals(2, result.getTotalElements());
+        assertEquals(2, result.getContent().size());
+        verify(profileRepository, times(1)).findAll(this.pageInput);
+        assertEquals(ProfileType.OWNER.getType(), result.getContent().getFirst().type());
+    }
+
+    @Test
+    void shouldReturnEmptyListWhenNoProfilesFound() {
+        var emptyPage = new PageOutputPort<Profile>(Collections.emptyList(), this.page, this.size, 0);
+
+        when(profileRepository.findAll(this.pageInput)).thenReturn(emptyPage);
+
+        var result = useCase.execute(this.pageInput);
+
+        assertNotNull(result);
+        assertEquals(0, result.getTotalElements());
+        assertTrue(result.getContent().isEmpty());
+    }
+
+    private Profile createProfile(Long id, String type) {
+        return Profile.create(id, type);
+    }
 }
