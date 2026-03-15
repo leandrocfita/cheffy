a2c62e4a5f29f132bb598f028e77d88ec055463b
thiagosslima <thiagosslima@MacBook-Pro-M4-Thiago-Soares.local>
2026-03-14 21:53:35 -0300

    fix: modificado classe paa o plural

--- src/main/java/br/com/fiap/cheffy/infrastructure/bean_config/ProfileUseCaseConfig.java
+++ src/main/java/br/com/fiap/cheffy/infrastructure/bean_config/ProfileUseCaseConfig.java
@@ -2,7 +2,7 @@ package br.com.fiap.cheffy.infrastructure.bean_config;
 
 import br.com.fiap.cheffy.application.profile.mapper.ProfileQueryMapper;
 import br.com.fiap.cheffy.application.profile.usecase.CreateProfileUseCase;
-import br.com.fiap.cheffy.application.profile.usecase.ListAllProfileUseCase;
+import br.com.fiap.cheffy.application.profile.usecase.ListAllProfilesUseCase;
 import br.com.fiap.cheffy.application.profile.usecase.UpdateProfileUseCase;
 import br.com.fiap.cheffy.domain.profile.port.output.ProfileRepository;
 import org.springframework.context.annotation.Bean;
@@ -19,10 +19,10 @@ public class ProfileUseCaseConfig {
     }
 
     @Bean
-    public ListAllProfileUseCase listAllProfilesUseCase(
+    public ListAllProfilesUseCase listAllProfilesUseCase(
             ProfileRepository profileRepository,
             ProfileQueryMapper mapper) {
-        return new ListAllProfileUseCase(profileRepository, mapper);
+        return new ListAllProfilesUseCase(profileRepository, mapper);
     }
 
     @Bean
